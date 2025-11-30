1ee87a08e7cb62d197cabd922a25f378e80c54cf
Chandan <chandanc01245@gmail.com>
2025-11-30 18:02:49 +0530

    Changed port number

--- backend/run.py
+++ backend/run.py
@@ -5,7 +5,7 @@ app = create_app()
 
 if __name__ == "__main__":
     # Get port from environment variable or default to 5000
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     debug = os.environ.get("FLASK_ENV", "production") != "production"
     
     app.run(host="0.0.0.0", port=port, debug=debug)
